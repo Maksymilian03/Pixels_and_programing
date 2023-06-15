898fb1d2d51d676e2548bcabd4e7db72be53210d
Maksymilian03 <maksymilianstolarek03@gmail.com>
2023-06-15 15:55:32 +0200

    Changed the ArticleCreateView function view

--- blog/views.py
+++ blog/views.py
@@ -7,14 +7,27 @@ from .forms import ArticleForm
 # Create your views here.
 
 
+def article_create_form(request):
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+        form.save(commit=True)
+        form = ArticleForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'form.html', context)
+
+
 class Main_page(ListView):
     template_name = 'main_page.html'
     model = Article
 
 
-class ArticleCreateView(PermissionRequiredMixin, CreateView):
-    template_name = 'form.html'
-    form_class = ArticleForm
-    success_url = reverse_lazy('index')
-    permission_required = 'blog.create_article'
+# class ArticleCreateView(PermissionRequiredMixin, CreateView):
+#     template_name = 'form.html'
+#     form_class = ArticleForm
+#     success_url = reverse_lazy('index')
+#     permission_required = 'blog.create_article'
 
