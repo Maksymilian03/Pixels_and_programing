904fe37ee5d2a2b70725ba40bb93018291e07e94
Maksymilian03 <maksymilianstolarek03@gmail.com>
2023-06-20 20:08:20 +0200

    changed display style in admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "ID", "jpg", "date"]
+
